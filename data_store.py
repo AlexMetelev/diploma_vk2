78dfa7a9f28a92ce8c1fec11eee3ba014555e178
AlexMetelev <126816248+AlexMetelev@users.noreply.github.com>
2023-06-29 10:57:07 +0700

    Update data_store.py

--- data_store.py
+++ data_store.py
@@ -40,7 +40,4 @@ def check_user(engine, profile_id, worksheet_id):
 
 if __name__ == '__main__':
     engine = create_engine(db_url_object)
-    Base.metadata.create_all(engine)
-    add_user(engine, 2113, 124512)
-    res = check_user(engine, 2113, 1245121)
-    print(res)
+    Base.metadata.create_all(engine)    
