3dd827ec32fdf6d83245c7a12d1fa12921fab902
AlexMetelev <126816248+AlexMetelev@users.noreply.github.com>
2023-06-29 10:54:47 +0700

    Update core.py

--- core.py
+++ core.py
@@ -6,9 +6,7 @@ from vk_api.exceptions import ApiError
 
 from config import acces_token
 
-
 # получение данных о пользователе
-
 
 class VkTools:
     def __init__(self, acces_token):
@@ -31,26 +29,21 @@ class VkTools:
             info = {}
             print(f'error = {e}')
 
-        print('pi')
-        pprint(info)
-
         result = {'name': (info['first_name'] + ' ' + info['last_name']) if
-        'first_name' in info and 'last_name' in info else None,
+                  'first_name' in info and 'last_name' in info else None,
                   'sex': info.get('sex'),
                   'city': info.get('city')['title'] if info.get('city') is not None else None,
                   'home_town': info.get('home_town') if info.get('home_town') is not None else None,
                   'year': self._bdate_toyear(info.get('bdate')),
-                  'relation': info.get('relation')
+                  'relation': info.get('relation') 
                   }
         return result
 
     def search_worksheet(self, params, offset):
         try:
-            pprint(params)
-            pprint(offset)
             users = self.vkapi.method('users.search',
                                       {
-                                          'count': 5,
+                                          'count': 50,
                                           'offset': offset,
                                           'hometown': params['city'],
                                           'sex': 1 if params['sex'] == 2 else 2,
@@ -64,13 +57,10 @@ class VkTools:
             users = []
             print(f'error = {e}')
 
-        # pprint(users)
-        result = [{'name': item['first_name'] + ' ' + item['last_name'],
-                   'id': item['id'],
+        result = [{'name': item['first_name'] + ' '+ item['last_name'],
+                   'id': item['id'], 
                    } for item in users['items'] if item['is_closed'] is False
                   ]
-        print('sw')
-        pprint(result)
         return result
 
     def get_photos(self, id):
@@ -91,23 +81,8 @@ class VkTools:
                    'comments': item['comments']['count']
                    } for item in photos['items']
                   ]
-        # pprint(result)
-        '''сортировка по лайкам и комментам'''
+        'сортировка по лайкам и комментам'
         result.sort(key=lambda x: x['likes'], reverse=True)
         # result.sort(key=lambda x: x['likes']+x['comments']*10, reverse=True)
-
+        
         return result[:3]
-
-
-if __name__ == '__main__':
-    user_id = 183447999
-    tools = VkTools(acces_token)
-    params = tools.get_profile_info(user_id)
-
-    pprint(params)
-
-    worksheets = tools.search_worksheet(params, 20)
-    worksheet = worksheets.pop()
-    photos = tools.get_photos(worksheet['id'])
-
-    pprint(worksheets)
